8e07150c2513f63a44d4b31a31e87789723feeaf
Oleksandr Savchenko <oleksandr.savchenko12@gmail.com>
2025-01-06 19:16:05 +0200

    Rest of the parts?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppHeaderComponent } from './header/app-header.component';
+import { AppFooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    AppHeaderComponent
-  ],
+    AppHeaderComponent,
+    AppFooterComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
 export class MainPageComponent {
+  services = [
+    { image: 'picture1.jpg', title: 'Бухгалтерський супровід ФОП' },
+    { image: 'picture2.jpg', title: 'Організація обліку та облікова політика' },
+    { image: 'picture3.jpg', title: 'Комплексне налаштування ПРРО' },
+    { image: 'picture4.jpg', title: 'Комплексна автоматизація бухгалтерського обліку' }
+  ];
 
+  reasons = [
+    'Професіоналізм: Наша команда складається з досвідчених бухгалтерів, які постійно підвищують свою кваліфікацію',
+    'Конфіденційність: Ми гарантуємо повну конфіденційність всієї інформації',
+    'Сучасні технології: Ми використовуємо сучасні програмні продукти для автоматизації бухгалтерських процесів',
+    'Індивідуальний підхід: Ми розробляємо індивідуальні рішення для кожного клієнта.'
+  ];
 }
